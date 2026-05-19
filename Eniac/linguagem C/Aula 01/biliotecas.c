5bfebf69ab2916f0b8d950162151a5f214049f42
Destroier22 <lucasccarlos222@gmail.com>
2026-05-19 00:17:54 -0300

    biblioteca

--- Eniac/linguagem C/Aula 01/biliotecas.c
+++ Eniac/linguagem C/Aula 01/biliotecas.c
@@ -6,11 +6,18 @@
 
 //biblioteca para comando LC_ALL para puxar o "portuguese"
 #include <locale.h>
-setlocale(LC_ALL, "portuguese");
-//biblioteca para manipulação de string
-#include <string.h>
-//strcmp = comparar string
 
-//strcpy = atribuir valor a string
-char teste[20];
-strcpy(teste, "teste 1 ok");
+#include <string.h>
+int main(){
+    //biblioteca para manipulação de string
+    setlocale(LC_ALL, "portuguese");
+
+    char nome1 [10], nome2 [10];
+    //strcmp = comparar string
+    strcmp();
+    //strcpy = atribuir valor a string
+    char teste [20];
+    strcpy(teste, "teste 1 ok");
+    system("pause");
+    return 0;
+}
